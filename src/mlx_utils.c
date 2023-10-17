275ff3f1388ad852f361ac3981b3bdacc789495b
Youssef <othmaniyoussef97@gmail.com>
2023-10-17 17:52:37 -0400

    change resize to false

--- src/mlx_utils.c
+++ src/mlx_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mlx_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yothmani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: yothmani <yothmani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 20:34:43 by yothmani          #+#    #+#             */
-/*   Updated: 2023/10/15 20:01:50 by yothmani         ###   ########.fr       */
+/*   Updated: 2023/10/17 17:52:20 by yothmani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ static void	render_objects(t_game *game)
 void	ft_render_window(t_game *game)
 {
 	game->mlx = mlx_init((game->map.width) * SIZE_IMG,
-			(game->map.height * SIZE_IMG) + 100, "so_long", true);
+			(game->map.height * SIZE_IMG) + 100, "so_long", false);
 	if (!game->mlx)
 		errror();
 	ft_create_texture(game);
